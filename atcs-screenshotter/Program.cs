9415fb22a7930392b64e23b3108b9bae00f0c620
Drew <vsts@internal.cloudapp.net>
2021-03-27 19:39:42 -0500

    Added logging for debug

--- atcs-screenshotter/Program.cs
+++ atcs-screenshotter/Program.cs
@@ -63,6 +63,8 @@ namespace atcs_screenshotter
                     // Always include the console logs
                     services.AddLogging(builder =>
                     {
+                        // Excessive logging limits
+                        builder.SetMinimumLevel(LogLevel.Debug);
                         builder.AddConsole();
                     });
 
